4cd2cb81a9bc1fa3ad46c5bc1a944274d5c5e5a3
josephjvine <joseph.vine@students.plymouth.ac.uk>
2023-05-09 11:12:35 +0100

    update

--- js/main.js
+++ js/main.js
@@ -5,13 +5,35 @@ let timerBool = false
 let timerComplete = false
 let timerPaused = false
 let timerPausedValue = 0
+let timerLengthInput;
+let numRoundsInput;
+let numRounds = 1;
+let roundCount = 0;
+let breaksCompleted = 0;
+let isBreak = false;
+let breakStart;
+let breakEnd;
+let currentBreak;
+let breakStarted;
+let breakLength =0;
+let numBreaks =0;
 
 
 function setup() {
     //create canvas of 800 x 800
     createCanvas(800, 800);
 
-    
+    timerLengthInput = createInput();
+    timerLengthInput.position(20, 100);
+
+    numRoundsInput = createInput();
+    numRoundsInput.position(20, 150);
+
+    breakDurationInput = createInput();
+    breakDurationInput.position(20, 160);
+
+    numBreaksInput = createInput();
+    numBreaksInput.position(20, 190);
 }
 
 
@@ -19,7 +41,18 @@ function start_timer() {
     timerPaused = false;
     timerBool = true;
     timerStart = millis();
+    timerLength = timerLengthInput.value(); // read timer length from input field
     console.log ("startbuttonpressed")
+    numRounds = numRoundsInput.value(); // read number of rounds from input field
+    roundCount = 0; // reset round count
+    timerLength = timerLengthInput.value();
+    let numRounds = numRoundsInput.value();
+    let breakDuration = breakDurationInput.value();
+    let numBreaks = numBreaksInput.value();
+    breaksCompleted = 0;
+    roundsCompleted = 0;
+    breakLength = breakLengthInput.value(); // read break length from input field
+    numBreaks = numBreaksInput.value(); // read number of breaks from input field
 }
 
 function pause_timer() {
@@ -35,6 +68,8 @@ function reset_timer() {
     timerPausedValue = 0;
     timerComplete = false;
     timerValue = 0;
+    roundsCompleted = 0;
+    breaksCompleted = 0;
     console.log ("resetbuttonpressed")
 
 }
@@ -66,40 +101,92 @@ function draw() {
     reset_button.position(20,200);
 
     reset_button.mousePressed(reset_timer);
+
+
+
+ //timer logic
+
+ //timer logic
+ if (timerPaused && timerBool){
     
+ }
+ else if (timerBool) {
+   timerValue = (millis() - timerStart) * 0.001;
+   let timeRemaining = round(timerLength - timerValue);
 
-    //timer logic
-    if (timerPaused && timerBool){
-        
-    }
-    else if (timerBool) {
-    timerValue = millis() -timerStart;
-    timerValue *=0.001;
-    timerValue = round(timerValue);
-    
-    }
+   if (timeRemaining <= 0) {
+     timerBool = false;
+     timerComplete = true;
+     currentRound++;
+   }
+ }
 
+ let totalTime = timerValue + timerPausedValue;
 
-    let totalTime=timerValue+timerPausedValue
-    if (totalTime==timerLength){
-        timerBool = false
-        timerComplete = true
-    }
+ // check if it's time for a break
+ if (currentRound > 0 && currentRound % roundsPerBreak == 0) {
+   if (!breakStarted) {
+     breakStart = millis();
+     breakStarted = true;
+   }
+   let breakTimeElapsed = round((millis() - breakStart) * 0.001);
+   let breakTimeRemaining = breakLength - breakTimeElapsed;
 
+   if (breakTimeRemaining <= 0) {
+     breakStarted = false;
+     currentBreak++;
+   } else {
+     fill(0, 102, 153);
+     textSize(50);
+     text("Break: " + breakTimeRemaining + " seconds", 20, 80);
+   }
+ } else {
+   fill(0, 102, 153);
+   textSize(50);
+   text("Round: " + currentRound + "/" + numRounds, 20, 80);
+   text("Timer length: " + timerLength + " seconds", 20, 150);
+   text("Breaks: " + currentBreak + "/" + numBreaks, 20, 220);
+   text(totalTime.toString(), 400, 400);
+ }
 
+ // check if it's time for a new round
+ if (currentRound <= numRounds) {
+   if (!timerBool && !timerComplete && !breakStarted) {
+     timerLength = round(timerLengthInput.value());
+     timerValue = 0;
+     timerPausedValue = 0;
+     timerBool = true;
+     timerComplete = false;
+     timerStart = millis();
+   }
+ } else {
+   fill(0, 102, 153);
+   textSize(50);
+   text("All rounds complete!", 20, 80);
+ }
 
-    //displaying timer
-    fill(0,102,153);
+ // displaying timer
+fill(0, 102, 153);
+textSize(50);
+text(
+  "Round" + (roundCount + 1) + "/" + numRounds + ", Time Left: " + (timerLength - totalTime),
+  20,
+  80
+);
+
+// timer complete text
+if (timerComplete) {
+    fill(0, 102, 153);
     textSize(50);
-    text(totalTime.toString(), 400,400);
-
-    //timer complete text
-    if (timerComplete){
-        fill(0,102,153);
-        textSize(50);
-        text("Well Done!", 450,450);
-
+    if (roundCount < numRounds - 1) {
+      text("Round " + (roundCount + 1) + " complete! Get ready for next round.", 150, 450);
+    } else {
+      text("Well Done!", 450, 450);
     }
+  }
 
 
 }
+
+
+
